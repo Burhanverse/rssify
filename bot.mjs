5df3af760cc6a2027585848e8aab72b7daae4eeb
Burhanverse <burhanverse@gmail.com>
2025-02-28 16:51:22 +0530

    Add `del_all` cmd

--- bot.mjs
+++ bot.mjs
@@ -9,6 +9,7 @@ import { startCmd } from './ext/commands/startHandler.mjs';
 import { statsCmd } from './ext/commands/statsHandler.mjs';
 import { aboutCmd } from './ext/commands/aboutHandler.mjs';
 import { checkSubscription } from './ext/isSubscribed.mjs';
+import { delAllCmd } from './ext/commands/delAllHandler.mjs';
 import { alertSender } from './ext/commands/alertSender.mjs';
 import { pauseCmd, resumeCmd } from './ext/commands/feedHandler.mjs';
 import { handleExport, handleImport } from './ext/commands/opmlHandler.mjs';
@@ -26,18 +27,19 @@ process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 await bot.api.setMyCommands([
-  { command: "start", description: "Start the bot.." },
-  { command: "add", description: "Add a new feed.." },
-  { command: "del", description: "Delete a feed.." },
-  { command: "list", description: "List of subscribed feeds.." },
-  { command: "set", description: "Set a group topic for feeds.." },
-  { command: "pause", description: "Pause feed updates.." },
-  { command: "resume", description: "Resume feed updates.." },
-  { command: "export", description: "Export feeds to OPML.." },
-  { command: "import", description: "Import feeds from OPML.." },
-  { command: "help", description: "Get some drugs.." },
-  { command: "stats", description: "Show bot server stats.." },
-  { command: "about", description: "Show information about the bot.." },
+  { command: "start", description: "Start RSS-ify..." },
+  { command: "add", description: "Add a new feed..." },
+  { command: "del", description: "Delete a feed..." },
+  { command: "list", description: "List of subscribed feeds..." },
+  { command: "set", description: "Set a group topic for feeds..." },
+  { command: "del_all", description: "Delete all feeds with backup..." },
+  { command: "pause", description: "Pause feed updates..." },
+  { command: "resume", description: "Resume feed updates..." },
+  { command: "export", description: "Export feeds to OPML..." },
+  { command: "import", description: "Import feeds from OPML..." },
+  { command: "help", description: "Get some drugs..." },
+  { command: "stats", description: "Show bot server stats..." },
+  { command: "about", description: "Show information about the bot..." },
 ]);
 
 bot.use(handleThreadId);
@@ -55,6 +57,7 @@ bot.callbackQuery(/^list_(prev|next)_(\d+)$/, spamProtection, isAdmin, handlePag
 bot.command('send', alertSender);
 bot.command('export', spamProtection, isAdmin, handleExport);
 bot.command('import', spamProtection, isAdmin, handleImport);
+bot.command('del_all', spamProtection, isAdmin, delAllCmd);
 
 (async () => {
   await connectDB();
